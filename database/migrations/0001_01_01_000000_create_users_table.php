80efaaa7f9e4f16ad2044af7210c7d7147588566
abdulfortech <ibnmudi@gmail.com>
2025-06-15 07:10:19 +0100

    database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('gender')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('state')->nullable();
+            $table->string('lga')->nullable();
+            $table->text('address')->nullable();
+            $table->string('role')->nullable(); 
+            $table->string('picture')->nullable();
+            $table->string('status')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
